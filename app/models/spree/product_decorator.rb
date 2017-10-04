74a0ba5e0e8eaf69b6e723c3f3bcce2f38340405
Simon Brook <simon@datanauts.co.uk>
2017-10-04 11:41:30 +0100

    Add bundle item discount factor to products for bundles

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,15 +1,8 @@
+# rubocop:disable Metrics/BlockLength
 Spree::Product.class_eval do
   has_many :parts, through: :assemblies_parts
   has_many :assemblies_parts, through: :variants_including_master,
-           source: :parts_variants
-
-  scope :individual_saled, -> { where(individual_sale: true) }
-
-  scope :search_can_be_part, ->(query){ not_deleted.available.joins(:master)
-    .where(arel_table["name"].matches("%#{query}%").or(Spree::Variant.arel_table["sku"].matches("%#{query}%")))
-    .where(can_be_part: true)
-    .limit(30)
-  }
+                              source: :parts_variants
 
   validate :assembly_cannot_be_part, if: :assembly?
 
@@ -31,8 +24,35 @@ Spree::Product.class_eval do
     errors.add(:can_be_part, Spree.t(:assembly_cannot_be_part)) if can_be_part?
   end
 
+  def bundle_item_discount_multiplier
+    return 1.0 unless assembly?
+    price / bundle_items_retail_price
+  end
+
   private
+
   def assemblies_part(variant)
-    Spree::AssembliesPart.get(self.id, variant.id)
+    Spree::AssembliesPart.get(id, variant.id)
+  end
+
+  def bundle_items_retail_price
+    assemblies_parts.includes(part: :default_price).map do |ap|
+      ap.count * ap.part.price
+    end.sum
+  end
+
+  class << self
+    def search_can_be_part(query)
+      not_deleted.available.joins(:master)
+                 .where(arel_table['name'].matches("%#{query}%")
+                 .or(Spree::Variant.arel_table['sku'].matches("%#{query}%")))
+                 .where(can_be_part: true)
+                 .limit(30)
+    end
+
+    def individual_saled
+      where(individual_sale: true)
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
